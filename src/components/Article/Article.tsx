82d29f7ae4f150b30d9649b1478ae9cdda01821f
System Administrator <root@Mymac.local>
2026-06-28 13:07:25 +0900

    “７章の課題2実装”

---
+++ src/components/Article/Article.tsx
@@ -0,0 +1,70 @@
+import { Fragment, useState, useEffect } from "react";
+import { Link, useParams } from 'react-router-dom';
+import { time } from "../../utils/time";
+import type { List } from '../../utils/list';
+
+export const Article = () => {
+
+  const { id } = useParams<string>();
+
+  const [post, setPost] = useState<List | null>(null);
+  const [load, setLoad] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetcher = async () => {
+      const res: Response = await fetch(`https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts/${(id)}`)
+      const data = await res.json()
+      setPost(data.post)
+      setLoad(!load)
+    }
+
+    fetcher()
+  }, [])
+
+
+  if (load) {
+    return <div className="mx-auto text-center mt-5">投稿読み込み中！！！</div>
+  } else if (!post) {
+    return <div>
+      <div className="mx-auto text-center mt-5">投稿が見つかりません</div>
+      <Link to="/" className="mx-auto text-blue-400 text-0xl" >記事一覧へ戻る</Link>
+    </div>
+  };
+
+
+
+  return (
+    <>
+      {
+        <Link to="/articles/:id">
+          <Fragment key={post.id} >
+            <main className="mx-auto max-w-3xl px-4 mt-3">
+              <div>
+                <img className="items-center" src={post.thumbnailUrl} /><br />
+              </div>
+
+              <div className="text-left">
+                <time dateTime={post.createdAt}>{time(new Date(post.createdAt))}</time>
+
+
+
+                <span>{post.categories?.map(category => (
+                  <span className="bg-gray-200 text-black rounded-2xl p-1" >{category}</span>
+                ))}
+                </span>
+
+
+                <h6 className="text-3xl mt-2">{post.title}</h6>
+
+                <div className="my-3" dangerouslySetInnerHTML={{ __html: post.content }}></div>
+              </div>
+
+              <Link to="/" className="text-blue-400" >記事一覧へ戻る</Link>
+            </main>
+          </Fragment>
+        </Link>
+      }
+    </>
+  );
+
+}
---
+++ src/utils/list.ts
@@ -0,0 +1,8 @@
+export type List = {
+  id: string,
+  thumbnailUrl: string,
+  createdAt: string,
+  categories: string[],
+  title: string,
+  content: string
+};
